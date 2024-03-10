956cc75c15c77e78d913e71231a1746c546f4269
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-03-10 17:38:57 -0500

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,26 +1,19 @@
- package edu.iastate.cs228.hw2;
-
-/**
- *  
- * @author
- *
- */
+//package edu.iastate.cs228.hw2;
+// @author Michael Jones V
 
 public class Point implements Comparable<Point>
 {
 	private int x; 
 	private int y;
 	
-	public static boolean xORy;  // compare x coordinates if xORy == true and y coordinates otherwise 
-	                             // To set its value, use Point.xORy = true or false. 
+	public static boolean xORy;  
+// compare x coordinates if xORy == true and y coordinates otherwise 
+// To set its value, use Point.xORy = true or false. 
 	
-	public Point()  // default constructor
-	{
+	public Point(){}  // default constructor
 		// x and y get default value 0
-	}
 	
-	public Point(int x, int y)
-	{
+	public Point(int x, int y) {
 		this.x = x;  
 		this.y = y;   
 	}
@@ -30,29 +23,20 @@ public class Point implements Comparable<Point>
 		y = p.getY();
 	}
 
-	public int getX()   
-	{
+	public int getX() {
 		return x;
 	}
 	
-	public int getY()
-	{
+	public int getY() {
 		return y;
 	}
 	
-	/** 
-	 * Set the value of the static instance variable xORy. 
-	 * @param xORy
-	 */
-	public static void setXorY(boolean xORy)
-	{
-		// TODO 
+	public static void setXorY(boolean xy) {
+		xORy = xy; 
 	}
 	
-	
 	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj) {
 		if (obj == null || obj.getClass() != this.getClass())
 		{
 			return false;
@@ -62,28 +46,21 @@ public class Point implements Comparable<Point>
 		return x == other.x && y == other.y;   
 	}
 
-	/**
-	 * Compare this point with a second point q depending on the value of the static variable xORy 
-	 * @param 	q 
-	 * @return  -1  if (xORy == true && (this.x < q.x || (this.x == q.x && this.y < q.y))) 
-	 *                || (xORy == false && (this.y < q.y || (this.y == q.y && this.x < q.x)))
-	 * 		    0   if this.x == q.x && this.y == q.y)  
-	 * 			1	otherwise 
-	 */
-	public int compareTo(Point q)
-	{
-		return 0; 
-		// TODO; 
+	public int compareTo(Point q) {
+    if (xORy == true && (this.x < q.x || (this.x == q.x && this.y < q.y))){
+      return -1;
+    } 
+    if (xORy == false && (this.y < q.y || (this.y == q.y && this.x < q.x))){
+      return -1;
+    } 
+    if (this.x == q.x && this.y == q.y){
+      return 0;
+    }
+    return 1;
 	}
 	
-	
-	/**
-	 * Output a point in the standard form (x, y). 
-	 */
 	@Override
-    public String toString() 
-	{
-		// TODO 
-		return null; 
+  public String toString() {
+		return "(" + x + ", " + y + ")";
 	}
 }
