f783279003a0a32b882975e983313115d132abca
MKJonesV <124622206+MKJonesV@users.noreply.github.com>
2024-03-10 17:35:14 -0500

    Update AbstractSorter.java

--- AbstractSorter.java
+++ AbstractSorter.java
@@ -1,10 +1,5 @@
-package edu.iastate.cs228.hw2;
-
-/**
- *  
- * @author
- *
- */
+//package edu.iastate.cs228.hw2;
+// @author Michael Jones V
 
 import java.util.Comparator;
 import java.io.FileNotFoundException;
@@ -12,42 +7,29 @@ import java.lang.IllegalArgumentException;
 import java.util.InputMismatchException;
 
 /**
- * 
  * This abstract class is extended by SelectionSort, InsertionSort, MergeSort, and QuickSort.
  * It stores the input (later the sorted) sequence. 
- *
  */
+
 public abstract class AbstractSorter
 {
 	
-	protected Point[] points;    // array of points operated on by a sorting algorithm. 
-	                             // stores ordered points after a call to sort(). 
+	protected Point[] points;
 	
-	protected String algorithm = null; // "selection sort", "insertion sort", "mergesort", or
-	                                   // "quicksort". Initialized by a subclass constructor.
+	protected String algorithm = null; 
 		 
 	protected Comparator<Point> pointComparator = null;  
-	
 	
 	// Add other protected or private instance variables you may need. 
 	
 
-	protected AbstractSorter()
-	{
-		// No implementation needed. Provides a default super constructor to subclasses. 
-		// Removable after implementing SelectionSorter, InsertionSorter, MergeSorter, and QuickSorter.
-	}
-	
-	
-	/**
-	 * This constructor accepts an array of points as input. Copy the points into the array points[]. 
-	 * 
-	 * @param  pts  input array of points 
-	 * @throws IllegalArgumentException if pts == null or pts.length == 0.
-	 */
+	protected AbstractSorter(){}
+
 	protected AbstractSorter(Point[] pts) throws IllegalArgumentException
 	{
-		// TODO 
+    points = pts;
+    if(pts == null || pts.length == 0)
+      throw new IllegalArgumentException("Invalid input array");
 	}
 
 		
@@ -70,49 +52,46 @@ public abstract class AbstractSorter
 	 */
 	public void setComparator(int order) throws IllegalArgumentException
 	{
-		// TODO 
+		if(order == 0){
+      pointComparator = new Comparator<Point>(){
+        
+        public int compare(Point p1, Point p2) {
+          p1.xORy = true;
+          return p1.compareTo(p2);
+        }
+      };
+    }
+    if(order == 1){
+      pointComparator = new Comparator<Point>(){
+        
+        public int compare(Point p1, Point p2) {
+          p1.xORy = false;
+          return p1.compareTo(p2);
+        }
+      };
+    }
+    else{
+      throw new IllegalArgumentException("Invalid order");
+    }
 	}
 
+	public abstract void sort(); // abstract class for sorting algoritms
 	
-
-	/**
-	 * Use the created pointComparator to conduct sorting.  
-	 * 
-	 * Should be protected. Made public for testing. 
-	 */
-	public abstract void sort(); 
-	
-	
-	/**
-	 * Obtain the point in the array points[] that has median index 
-	 * 
-	 * @return	median point 
-	 */
-	public Point getMedian()
+	public Point getMedian() // returns median point
 	{
 		return points[points.length/2]; 
 	}
 	
 	
-	/**
-	 * Copys the array points[] onto the array pts[]. 
-	 * 
-	 * @param pts
-	 */
-	public void getPoints(Point[] pts)
+	public void getPoints(Point[] pts) // Copies the array
 	{
-		// TODO 
+		pts = points;
 	}
 	
-
-	/**
-	 * Swaps the two elements indexed at i and j respectively in the array points[]. 
-	 * 
-	 * @param i
-	 * @param j
-	 */
-	protected void swap(int i, int j)
+	protected void swap(int i, int j) // Swaps the points at indexes i and j
 	{
-		// TODO 
+		Point p = points[i];
+    points[i] = points[j];
+    points[j] = p;
 	}	
 }
